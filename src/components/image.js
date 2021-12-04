468d70ee2d97c23d9a177e80b0b883bfe3348be2
sromero50 <seba98asd@hotmail.com>
2021-12-04 14:11:23 -0300

    unused files deleted

--- src/components/image.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Card from 'react-bootstrap/Card'
-const Image = (props) => {
-    return (
-    <Card.Img variant="top" src={props.image} />
-    )
-}
-
-export default Image;
